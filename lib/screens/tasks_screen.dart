ebcbee791d7640d805bfc54dc4f05d88dfa5ddc2
ayanayan <ayansadhukhan28@gmail.com>
2020-11-21 18:55:44 +0530

    UI updation Commit

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,44 +5,59 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60.0, left: 30, right: 30, bottom: 30),
-        child: Column(
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            CircleAvatar(
+        Container(
+          padding: EdgeInsets.only(top: 60.0, left: 30, right: 30, bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
                 backgroundColor: Colors.white,
                 radius: 30,
-                child: Icon(Icons.list,
-                size: 30,
-                color: Colors.lightBlueAccent,
-                ),),
-            SizedBox(height: 20.0,),
-            Text('My Notes',
-            style: TextStyle(
-                color: Colors.white,
-              fontWeight: FontWeight.w700,
-               fontSize: 50,
-            ),
-            ),
-            Text('12 Tasks',
-            style: TextStyle(
-                color: Colors.white,
-              fontSize: 18,
-            ),
-            ),
-            Container(
-              
-              height: 300.0,
-              decoration: BoxDecoration(
-                color: Colors.white, 
-                borderRadius: BorderRadius.circular(10),
-                
+                child: Icon(
+                  Icons.list,
+                  size: 30,
+                  color: Colors.lightBlueAccent,
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'My Notes',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 50,
+                ),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
